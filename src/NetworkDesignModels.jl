4d6cca2e2959e2ecbb2b3153637c1ead611e9cd4
Yeesian Ng <ngyeesian@gmail.com>
2018-05-01 15:35:40 -0400

    switch to new API

--- src/NetworkDesignModels.jl
+++ src/NetworkDesignModels.jl
@@ -1,9 +1,9 @@
 module NetworkDesignModels
 
-    import JuMP, Gurobi, TransitNetworks
+    import JuMP, Gurobi, TransitNetworks; const TN = TransitNetworks
 
     mutable struct NetworkCoverageModel
-        np::TransitNetworks.TransitNetworkProblem
+        np::TN.TransitNetworkProblem
         model::JuMP.Model
         budget::JuMP.Variable
         x::Vector{JuMP.Variable}
@@ -11,29 +11,28 @@ module NetworkDesignModels
     end
 
     function NetworkCoverageModel(
-            np::TransitNetworks.TransitNetworkProblem;
+            np::TN.TransitNetworkProblem;
             initialbudget::Int=0,
-            solver = Gurobi.GurobiSolver(OutputFlag=0)
+            solver = Gurobi.GurobiSolver()
         )
         model = JuMP.Model(solver=solver)
         JuMP.@variable(model, x[l=1:length(np.lines)], Bin)
         JuMP.@variable(model, budget)
         JuMP.@variable(model,
-            θ[u=1:np.nstations,v=TransitNetworks.dests(np,u),
-              r=1:length(np.stage[u,v])] >= 0
+            θ[u=1:np.nstations,v=TN.dests(np,u), r=1:TN.nroutes(np,u,v)] >= 0
         )
         JuMP.@objective(model, Max, sum(sum(
-                np.odmatrix[u,v]*sum(θ[u,v,r] for r in 1:length(np.stage[u,v]))
-                for v in TransitNetworks.dests(np, u)
+                np.odmatrix[u,v]*sum(θ[u,v,r] for r in 1:TN.nroutes(np,u,v))
+                for v in TN.dests(np, u)
             )
             for u in 1:np.nstations
         ))
-        JuMP.@constraint(model, [u=1:np.nstations, v=TransitNetworks.dests(np,u)],
-            sum(θ[u,v,r] for r in 1:length(np.stage[u,v])) <= 1
+        JuMP.@constraint(model, [u=1:np.nstations, v=TN.dests(np,u)],
+            sum(θ[u,v,r] for r in 1:TN.nroutes(np,u,v)) <= 1
         )
         JuMP.@constraint(model,
-            [u=1:np.nstations, v=TransitNetworks.dests(np,u),
-             r=1:length(np.stage[u,v]), s=np.stage[u,v][r]],
+            [u=1:np.nstations, v=TN.dests(np,u),
+             r=1:TN.nroutes(np,u,v), s=TN.stages(np,u,v,r)],
             θ[u,v,r] <= sum(x[l] for l in 1:length(np.lines) if np.linesegments[l,s])
         )
         JuMP.@constraint(model, sum(x) <= budget)
